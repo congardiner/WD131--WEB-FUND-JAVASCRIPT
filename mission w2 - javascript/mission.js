9ee33dc3f0db6fbdf4d43a3804ec7ee9e1c17661
congardiner <gar21051@byui.edu>
2024-09-25 11:34:09 -0600

    Edited: my src img was spelled incorrectly for dark mode, all transitions are now properly in place.

--- mission w2 - javascript/mission.js
+++ mission w2 - javascript/mission.js
@@ -8,10 +8,12 @@ const logoImage = document.querySelector('footer img')
 changeTheme.addEventListener('change', () => {
     if (changeTheme.value === 'dark') {
         mainDiv.classList.add('dark-theme');
-        logoImage.src = 'byui-logo_blue.webp';
+        logoImage.src = 'byui-logo_white.png';
+        // need to change the photo to match theme
 
     } else {
         mainDiv.classList.remove('dark-theme');
-        logoImage.src = 'byui-logo_white.webp';
+        logoImage.src = 'byui-logo_blue.webp';
+        // need to change the photo to match theme
     }
 });
