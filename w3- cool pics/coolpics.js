bfebb77e8b50c25e23c67b5b258d60ab52ab50ca
congardiner <gar21051@byui.edu>
2024-10-01 15:54:13 -0600

    Committed Changes within the coolpics library, ensuring that content meets requirements for sizing and media queries.

--- w3- cool pics/coolpics.js
+++ w3- cool pics/coolpics.js
@@ -1,2 +1,6 @@
 // javascript for my coolpics landing page practice //
 
+// need to make a hamburger menu that is collapsable and usable within the varying screen restrictions
+// for mobile and desktop usage 
+
+
