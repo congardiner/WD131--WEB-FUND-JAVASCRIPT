2aa3c37c7315b58e502461daa07c49f7ad9ad49b
congardiner <gar21051@byui.edu>
2024-10-02 09:57:19 -0600

    Browser Windows for the image, header, and nav bar have all been updated for responsiveness within my css, and html. Js to come soon!

--- w3- cool pics/coolpics.js
+++ w3- cool pics/coolpics.js
@@ -3,4 +3,10 @@
 // need to make a hamburger menu that is collapsable and usable within the varying screen restrictions
 // for mobile and desktop usage 
 
+const menuButton = document.getElementById('menu-button');
+const navMenu = document.getElementById('nav-menu');
 
+menuButton.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+    
+});
