a7bc2fec44446945be59fc7787e26f075db727a0
congardiner <gar21051@byui.edu>
2024-12-09 09:39:14 -0700

    Includes updated changes made to my webpages, as I made a terms and conditions section, along with a privacy webpage.

--- personalsite/site.mjs
+++ personalsite/site.mjs
@@ -1,2 +1,5 @@
 // Need to create an index or array of content that can then be loaded into my webpage from the mjs file that will dynamically load the content that I have so that it works
 
+// this could become a quote index, so that it stores all of this from an ES Modules standpoint before integrating the other elements that I need to.
+
+
